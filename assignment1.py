b0c5179a47b91fa4de74df3d195500d11c2f2c6a
Abdelrahmanmemara <119069265+Abdelrahmanmemara@users.noreply.github.com>
2024-06-11 14:33:48 +0200

    Final update

--- assignment1.py
+++ assignment1.py
@@ -18,9 +18,7 @@ class Aggregation(Config):
     Tjoin:float = 0.3 + transformed_number
     # The time elapse to leave an
     Tleave:float = 0.5 + transformed_number
-    # D times for checking the nr. of neighbors to leave the aggregation.
     delta_time: float = 0.5 
-    threshold:float = 0.005
 
     def weights(self) -> tuple[float, float, float]:
         return (self.Tjoin, self.Tleave, self.D)
@@ -28,22 +26,35 @@ class Aggregation(Config):
 
 class Cockroach(Agent):
     config: Aggregation
+    # The starting state of the agent
     state:str = 'wandering'
+    # D times for checking the nr. of neighbors to leave the aggregation.
     D:int = 0
 
     def update(self):
-
+        # If the agent state is wandering, then we will keep moving randomly and checking if we can join anything.
         if self.state == 'wandering':
+            # Move Randomly
             self.change_position()
+            # Check if we can join any aggregation
             self.joining()
+        # The we are joining an aggregation
         if self.state == 'joining':
-            print('yes')
+            # If we can join an aggregation we can not stop straight away we move a little and then stop
             self.pos += self.move * self.config.Tjoin
+            # We then change the posiiton to still
             self.state = 'still'
+        
         if self.state == 'still':
-            self.still(self.D)
+            # We apply the still() function which freezes the movement of the agent, and allows to move it specific to the instructions in the assignment.
             self.D += 1
+            self.still(self.D)
+            # The nr of iterations in which it was frozen
+            if self.D == 50:
+                # We set the agents iterations back to zero once it moves again
+                self.D = 0
         if self.state == 'leaving':
+            # If it is leaving it has to move for some time until it can be wandering again.
             self.pos += self.move * self.config.Tleave
             self.state = 'wandering'
                 
@@ -57,6 +68,7 @@ class Cockroach(Agent):
             self.state= 'joining'
     def still(self, D):
         self.freeze_movement()
+        # D times for checking the nr. of neighbors to leave the aggregation.
         if D == 50:
             neighbors = list(self.in_proximity_accuracy())
             prob = len(neighbors) / 50
